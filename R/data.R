b3a5e357fc851c06c1aef855d3b0cd6bc0603aad
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2020-08-03 11:07:35 -0300

    vignette fixed and gitignore updated

--- R/data.R
+++ R/data.R
@@ -1,5 +1,7 @@
 #' List of earthquakes since B.C. around the world from NOAA
 #'
+#' Help to access data: https://stackoverflow.com/questions/32964741/accessing-sysdata-rda-within-package-functions
+#'
 #' @source The Significant Database Earthquake from NOAA
 #' \url{https://www.ngdc.noaa.gov/nndc/struts/form?t=101650&s=1&d=1}
 #'
