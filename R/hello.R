e22fbc78f5fbb4fd16bc606840fa2a7895bc195c
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2020-09-12 11:11:06 -0300

    ignore .Rproj.user

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,7 @@
+# https://stackoverflow.com/questions/17107206/change-temporary-directory
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
